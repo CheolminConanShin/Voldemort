0b1dc73cbef18d0672d744900d2f670b56efbf34
CheolminConanShin <scm0226@gmail.com>
2017-05-21 16:32:48 +0900

    - instead of using subComponent support class, just mocked specific lifecycle method (render)

--- __tests__/components/slide/slideContainerSpec.js
+++ __tests__/components/slide/slideContainerSpec.js
@@ -1,13 +1,11 @@
 import React from "react";
 import {mount} from "enzyme";
-import {stubComponent} from "../../__mocks__/stubComponent";
 import configureStore from "redux-mock-store";
 import {Provider} from "react-redux";
 import SlideContainer from "../../../client/components/slide/slideContainer";
 import SlidePresenter from "../../../client/components/slide/slidePresenter";
 
 describe('<SlideContainer/>', () => {
-    stubComponent(SlidePresenter)
 
     const initialState = {
         slidesReducer: {
@@ -22,10 +20,9 @@ describe('<SlideContainer/>', () => {
     }
 
     const store = configureStore()(initialState)
-
-    // const renderedElement = mount(<Provider store={store}><SlideContainer/></Provider>)
+    SlidePresenter.prototype['render'] = jest.fn()
+    const renderedElement = mount(<Provider store={store}><SlideContainer/></Provider>)
     it('should render slide presenter', () => {
-        expect(true).toBeTruthy()
-        // const slidePresenter = renderedElement.find('div')
+        expect(renderedElement.find('SlidePresenter').exists()).toBeTruthy()
     })
 })
