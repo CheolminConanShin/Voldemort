3673872ca19236877e203365bcf359b37df2fc4a
CheolminConanShin <scm0226@gmail.com>
2017-05-25 14:54:04 +0900

    - Progress Card component refactored and tested

--- __tests__/components/slide/pager/page/cardUsage/progressCardSpec.js
+++ __tests__/components/slide/pager/page/cardUsage/progressCardSpec.js
@@ -16,6 +16,26 @@ describe('<ProgressCard/>', () => {
 
     const renderedElement = shallow(<ProgressCard cardName={initialProperty.cardName} cardInfo={initialCardInfo} openCalculator={initialProperty.openCalculator}/>)
 
+    it('should return left amount object containing manwon, cheonwon, baekwon field on getLeftAmount method', () => {
+        const progressCard = renderedElement.instance()
+        const expectedResult = {
+            leftManwon: 0,
+            leftCheonwon: 5,
+            leftBaekwon:0
+        }
+        expect(progressCard.getLeftAmount("10000", "5000")).toEqual(expectedResult)
+    })
+
+    it('should return left amount string in form of xxxx원 부족 on leftAmountToString method', () => {
+        const progressCard = renderedElement.instance()
+        const leftAmount = {
+            leftManwon: 5,
+            leftCheonwon: 5,
+            leftBaekwon: 5
+        }
+        expect(progressCard.leftAmountToString(leftAmount)).toEqual(" 5만 5천 5백원 부족")
+    })
+
     it('should display card name and left amount string', () => {
         let cardNameField = renderedElement.childAt(0).text();
         expect(cardNameField).toEqual('코난의 카드')
