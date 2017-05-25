c2368f192a70181424dd584704dd86bd65970636
CheolminConanShin <scm0226@gmail.com>
2017-05-25 16:25:34 +0900

    - [WIP] Satisfied

---
+++ __tests__/components/slide/pager/page/cardUsage/satisfiedCardSpec.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {shallow} from 'enzyme'
+
+import SatisfiedCard from '../../../../../../client/components/slide/pager/page/cardUsage/satisfiedCard'
+
+describe('<SatisfiedCard/>', () => {
+    const renderedElement = shallow('<SatisfiedCard/>')
+    it('should display card name and satisfied string and deposit date', () => {
+        expect(renderedElement.)
+    })
+})
