5640328b3737d513957a109511c402ebd3e860df
CheolminConanShin <scm0226@gmail.com>
2017-06-20 22:19:08 +0900

    GOAL: left amount field is calculating its own left amount string within its method

--- __tests__/components/pager/history/leftAmountFieldSpec.js
+++ __tests__/components/pager/history/leftAmountFieldSpec.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import LeftAmountField from '../../../../client/components/pager/history/leftAmountField'
+import configureStore from 'redux-mock-store'
 import {shallow} from "enzyme";
 
 describe('<LeftAmountField/>', () => {
+    const initState = {
+        calculatorReducer: {
+            cardInfo: {
+                total: 100000,
+                used: 14000
+            }
+        }
+    }
+
+    const store = configureStore()(initState)
 
     const leftAmountDate = "2017.07 (07.01 ~ 07.31)"
-    const leftAmount = "8만 6천원 부족"
-    const renderedElement = shallow(<LeftAmountField date={leftAmountDate} amount={leftAmount}/>)
+    const leftAmount = " 8만 6천원 부족"
+    const renderedElement = shallow(<LeftAmountField store={store} date={leftAmountDate}/>).shallow()
 
     it('should display left amount with specific date', function () {
         expect(renderedElement.find("#date").text()).toEqual(leftAmountDate)
--- client/components/pager/history/leftAmountField.js
+++ client/components/pager/history/leftAmountField.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {connect} from 'react-redux'
 
 const styles = {
     container: {
@@ -24,14 +25,36 @@ const styles = {
     }
 }
 
+const mapStateToProps = (state) => {
+    return {
+        cardInfo: state.calculatorReducer.cardInfo
+    }
+}
+
+@connect(mapStateToProps)
 export default class LeftAmountField extends React.Component {
     render() {
+        const {used, total} = this.props.cardInfo
+        const leftAmount = this.leftAmountToString(total, used)
         return (
             <div style={styles.container}>
                 <span style={styles.date} id="date">{this.props.date}</span>
                 <span style={styles.range} id="range">{this.props.range}</span>
-                <span style={styles.amount} id="amount">{this.props.amount}</span>
+                <span style={styles.amount} id="amount">{leftAmount}</span>
             </div>
         )
+    }
+
+    leftAmountToString(total, used) {
+        const leftAmount = total - used
+        const leftManwon = parseInt(leftAmount / 10000)
+        const leftCheonwon = parseInt(leftAmount % 10000 / 1000)
+        const leftBaekwon = parseInt(leftAmount % 1000 / 100)
+
+        let oneMillionLeft = leftManwon > 0 ? " " + leftManwon + "만" : "";
+        let oneThousandLeft = leftCheonwon > 0 ? " " + leftCheonwon + "천" : "";
+        let oneHundredLeft = leftBaekwon > 0 ? " " + leftBaekwon + "백" : "";
+
+        return oneMillionLeft + oneThousandLeft + oneHundredLeft + "원 부족"
     }
 }
